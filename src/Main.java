365eab95910ef9b3ca632c9f52e80889fdc38ede
Accute9 <aneeshman07@gmail.com>
2026-09-02 21:18:25 -0400

    Wrote code for algorithm that passes preliminary tests based on hand-written pseudocode

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
   //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        if (args.length != 3) {
+            System.out.println("Incorrect number of arguments, please try again following this format: # month ");
+            System.out.println();
+        } else {
+            // All CLI arguments come as strings, so need to parse to integers before calling runSimulation
+            int months = Integer.parseInt(args[0]);
+            int rabbits = Integer.parseInt(args[1]);
+            int wolves = Integer.parseInt(args[2]);
+            runSimulation(months, rabbits, wolves);
         }
     }
+
+    public static void runSimulation(int months, int rabbits, int wolves) {
+        System.out.println("Your program begins with " + wolves + " wolves and " + rabbits +  " rabbits and runs for " + months + " months. \n");
+        while (months > 0) {
+            int wolvesLeft = 0;
+            while (rabbits > 0 && wolvesLeft < wolves) {
+                rabbits -= 1;
+                wolvesLeft += 1;
+            }
+            if (rabbits > 0) {
+                int secondWolvesCount = 0;
+                while (secondWolvesCount < wolves && rabbits > 0) {
+                    rabbits -= 1;
+                    secondWolvesCount += 1;
+                }
+            }
+            wolves = wolvesLeft;
+            wolves += Math.floorDiv(wolves, 2);
+            rabbits += (5 * Math.floorDiv(rabbits, 2));
+            months -= 1;
+        }
+        System.out.println("At the end of the simulation, there are " + wolves + " wolves and " + rabbits + " rabbits.\n");
+    }
+
 }
